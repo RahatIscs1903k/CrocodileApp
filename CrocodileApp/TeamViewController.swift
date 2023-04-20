556b61e0725bc464449d0342c8ba813819a3e811
Evgeniya Maximova <onaxiz@tutanota.com>
2023-04-20 09:54:48 +0300

    add dynamic changing of teamNames

--- CrocodileApp/TeamViewController.swift
+++ CrocodileApp/TeamViewController.swift
@@ -12,11 +12,34 @@ class TeamViewController: UIViewController {
     @IBOutlet weak var circle1: UIView!
     
     @IBOutlet weak var circle2: UIView!
+    
+    @IBOutlet weak var emoji1: UILabel!
+    
+    @IBOutlet weak var emoji2: UILabel!
+    
+    @IBOutlet weak var teamName1: UILabel!
+    
+    @IBOutlet weak var teamName2: UILabel!
+    
+    
+    struct Team {
+        let teamBackgroundColor: UIColor
+        let teamEmoji: String
+        let teamName: String
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let teams = Array<Team>()
+        
+        let teamCowboys = Team(teamBackgroundColor: UIColor.green, teamEmoji: "🤠 ", teamName: "Ковбои")
+        
+        
         circle1.layer.cornerRadius = 28
         circle2.layer.cornerRadius = 28
+        circle1.backgroundColor = teamCowboys.teamBackgroundColor
+        emoji1.text = teamCowboys.teamEmoji
+        teamName1.text = teamCowboys.teamName
     }
 }
 
