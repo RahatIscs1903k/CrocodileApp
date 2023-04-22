9931455da85cf311a453d07773c10ef026509150
Evgeniya Maximova <onaxiz@tutanota.com>
2023-04-22 18:19:56 +0300

    CategoriesViewController

---
+++ CrocodileApp/Controllers/CategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesViewController.swift
+//  CrocodileApp
+//
+//  Created by Evgeniia Maksimova on 22.04.2023.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
