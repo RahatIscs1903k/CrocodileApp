dd8bf9d1655e8f13e46af290f904fabb432adcba
Evgeniya Maximova <onaxiz@tutanota.com>
2023-04-22 22:19:27 +0300

    images for categories

--- CrocodileApp/Controllers/Categories/CategoriesViewController.swift
+++ CrocodileApp/Controllers/Categories/CategoriesViewController.swift
@@ -10,28 +10,47 @@ import UIKit
 class CategoriesViewController: UIViewController {
     
     //MARK: - Outlets
-    @IBOutlet weak var teamCard1View: UIView!
-    @IBOutlet weak var teamCard1TeamNameLabel: UILabel!
-    @IBOutlet weak var teamCard1CustomView: UIView!
-    @IBOutlet weak var teamCard1EmogiLabel: UILabel!
+    @IBOutlet weak var categoriesCard1View: UIView!
+    @IBOutlet weak var categoriesCard1TeamNameLabel: UILabel!
+    @IBOutlet weak var categoriesCard1CustomView: UIView!
+    @IBOutlet weak var categoriesCard1EmogiLabel: UILabel!
     
-    @IBOutlet weak var teamCard2View: UIView!
-    @IBOutlet weak var teamCard2TeamNameLabel: UILabel!
-    @IBOutlet weak var teamCard2CustomView: UIView!
-    @IBOutlet weak var teamCard2EmogiLabel: UILabel!
+    @IBOutlet weak var categoriesCard2View: UIView!
+    @IBOutlet weak var categoriesCard2TeamNameLabel: UILabel!
+    @IBOutlet weak var categoriesCard2CustomView: UIView!
+    @IBOutlet weak var categoriesCard2EmogiLabel: UILabel!
     
-    @IBOutlet weak var teamCard3View: UIView!
-    @IBOutlet weak var teamCard3TeamNameLabel: UILabel!
-    @IBOutlet weak var teamCard3CustomView: UIView!
-    @IBOutlet weak var teamCard3EmogiLabel: UILabel!
+    @IBOutlet weak var categoriesCard3View: UIView!
+    @IBOutlet weak var categoriesCard3TeamNameLabel: UILabel!
+    @IBOutlet weak var categoriesCard3CustomView: UIView!
+    @IBOutlet weak var categoriesCard3EmogiLabel: UILabel!
     
-    @IBOutlet weak var teamCard4View: UIView!
-    @IBOutlet weak var teamCard4TeamNameLabel: UILabel!
-    @IBOutlet weak var teamCard4CustomView: UIView!
-    @IBOutlet weak var teamCard4EmogiLabel: UILabel!
+    @IBOutlet weak var categoriesCard4View: UIView!
+    @IBOutlet weak var categoriesCard4TeamNameLabel: UILabel!
+    @IBOutlet weak var categoriesCard4CustomView: UIView!
+    @IBOutlet weak var categoriesCard4EmogiLabel: UILabel!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let firstCategoryCard = CardWithRoundImage(circle: categoriesCard1CustomView, emoji: categoriesCard1EmogiLabel, cardName: categoriesCard1TeamNameLabel, card: categoriesCard1View)
+        
+        let secondCategoryCard = CardWithRoundImage(circle: categoriesCard2CustomView, emoji: categoriesCard2EmogiLabel, cardName: categoriesCard2TeamNameLabel, card: categoriesCard2View)
+        
+        let thirdCategoryCard = CardWithRoundImage(circle: categoriesCard3CustomView, emoji: categoriesCard3EmogiLabel, cardName: categoriesCard3TeamNameLabel, card: categoriesCard3View)
+        
+        let fourthCategoryCard = CardWithRoundImage(circle: categoriesCard4CustomView, emoji: categoriesCard4EmogiLabel, cardName: categoriesCard4TeamNameLabel, card: categoriesCard4View)
+        
+        let firscCategory = Categories().chooseRandomCatgoryCard()
+        var secondCategory = Categories().chooseRandomCatgoryCard()
+        let thirdCategory = Categories().chooseRandomCatgoryCard()
+        let fourthCategory = Categories().chooseRandomCatgoryCard()
+        
+
+        
+        firstCategoryCard.drowCard(context: firscCategory)
+        secondCategoryCard.drowCard(context: secondCategory)
+    
     }
 }
--- CrocodileApp/Controllers/Teams/TeamViewController.swift
+++ CrocodileApp/Controllers/Teams/TeamViewController.swift
@@ -28,9 +28,9 @@ class TeamViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstTeamCard = CardWithRoundImage(circle: circle1, emoji: emoji1, teamName: teamName1, card: teamCard1)
+        let firstTeamCard = CardWithRoundImage(circle: circle1, emoji: emoji1, cardName: teamName1, card: teamCard1)
         
-        let secondTeamCard = CardWithRoundImage(circle: circle2, emoji: emoji2, teamName: teamName2, card: teamCard2)
+        let secondTeamCard = CardWithRoundImage(circle: circle2, emoji: emoji2, cardName: teamName2, card: teamCard2)
         
         let firstTeam = Teams().chooseRandomTeamCard()
         var secondTeam = Teams().chooseRandomTeamCard()
