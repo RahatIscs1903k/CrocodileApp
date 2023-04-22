e65a18639728700a29442a7086fee532214a227a
Evgeniya Maximova <onaxiz@tutanota.com>
2023-04-22 23:37:22 +0300

    not-working-no-initializer

--- CrocodileApp/Customs/CardWithRoundImage.swift
+++ CrocodileApp/Customs/CardWithRoundImage.swift
@@ -12,8 +12,10 @@ class CardWithRoundImage {
     var emoji: UILabel
     var cardName: UILabel
     var card: UIView
+    var isChecked: Bool = false
     
-    init(circle: UIView, emoji: UILabel, cardName: UILabel, card: UIView) {
+    
+    init(circle: UIView = UIView(), emoji: UILabel = UILabel(), cardName: UILabel = UILabel(), card: UIView = UIView()) {
         self.circle = circle
         self.emoji = emoji
         self.cardName = cardName
